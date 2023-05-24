561c6b457eeefd2c7abff810abaf9b3153e270b4
Mariano-Pozo <mlxito.nax2@gmail.com>
2023-05-23 22:09:46 -0300

    Se agrega pp

---
+++ parcial.py
@@ -0,0 +1,39 @@
+#Pozo, Mariano Leonel
+import re
+import csv
+import json
+
+with open('C:\\Users\\Inoue\\Desktop\\carpet\\Programacion_I\\Parcial\\dt.json') as archivo:
+    data_nba = json.load(archivo)
+          
+lista_nba = data_nba["jugadores"]
+
+def mostrar_jugadores(lista):
+    for jugador in lista:
+        print("{} - {}".format(jugador["nombre"],jugador["posicion"]))
+
+def mostrar_estadistica(lista):
+    cantidad = len(lista)
+    indice_ingresado = int(input("Ingresar indice del jugador para ver sus estadisticas: "))
+    while indice_ingresado > cantidad or indice_ingresado < 0:
+        indice_ingresado = int(input("Ingresar indice valido del jugador para ver sus estadisticas"))
+
+def ingresar_opcion(opciones: list)-> str:
+    while True:
+        opcion = input("Igresar una de las siguientes opciones:  {opciones}").lower()
+        if opcion not in opciones:
+            print("Opcion no valida. Try again")
+        else:
+            return opcion
+
+
+
+print(lista_nba[-1])
+
+
+
+
+
+'''print (type (data_nba))
+print (data_nba)'''
+
